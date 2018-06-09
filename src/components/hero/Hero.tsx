90882d654670c7d7ce069221f705a47dc535cb8c
Sasha Bajzek <dabridginator@gmail.com>
2018-06-09 09:50:24 -0700

    Add initial animated token

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { TextData } from '../../api/texts';
 
+import AnimatedToken from '../animated/Token';
+
 export default function Hero(props: { text: TextData, textId: string, tokenVersion?: string }) {
   const tokenTextObj = props.text.value.find((text: any) => {
     return text.label === props.textId;
@@ -9,6 +11,7 @@ export default function Hero(props: { text: TextData, textId: string, tokenVersi
   return (
     <div className="website-hero">
       <div className={`website-hero__logo ${props.tokenVersion}`}>
+        {props.tokenVersion !== 'purple' ? <AnimatedToken/> : ''}
         <div className="website-hero__text">
           {tokenText}
         </div>
